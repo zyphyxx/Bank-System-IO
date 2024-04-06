c7e63113c9b6ea33d297102ac376e96bb7b31ee6
Edgar Jesus <mendesbr92@gmail.com>
2024-04-06 14:50:51 -0300

    inicio do preparo para migrar para um projeto spring boot

--- banksimulator/src/view/Rodando.java
+++ banksimulator/src/view/Rodando.java
@@ -7,19 +7,26 @@ import java.util.Scanner;
 
 public class Rodando {
 
+    private Account account = new Account();
+    private Scanner sc = new Scanner(System.in);
+    private BankAccountOperationsService bs = BankAccountOperationsService.getInstance();
+
     public static void main(String[] args) {
 
-        Account account = new Account();
-        Scanner sc = new Scanner(System.in);
-        BankAccountOperationsService bs = BankAccountOperationsService.getInstance();
+        Rodando bk = new Rodando();
 
         System.out.println("digite seu nome");
-        account.setName(sc.nextLine());
+        bk.account.setName(bk.sc.nextLine());
 
         System.out.println("Digite o deposito:");
+        bk.bs.deposit(bk.sc.nextBigDecimal());
+
+        System.out.println("digite o valor de saque:");
+        bk.bs.withdraw(bk.sc.nextBigDecimal());
 
 
+        System.out.println(bk.account.getName());
+        System.out.println(bk.bs.balance());
 
-        System.out.println(account.getName());
     }
 }
