6dcbaeb768c6ae548909e92f94c7848debbfe4af
Edgar Jesus <mendesbr92@gmail.com>
2024-04-19 18:45:14 -0300

    criado a validação com senha na transferencia

--- bank-system-io/src/main/java/com/banksystemio/banksystem/services/AccountService.java
+++ bank-system-io/src/main/java/com/banksystemio/banksystem/services/AccountService.java
@@ -6,6 +6,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -59,5 +60,10 @@ public class AccountService {
         } else {
             System.out.println("Usuario não existe");
         }
+    }
+
+    public BigDecimal getBalance (Long id) {
+        Optional<Account> accountById = findAccountById(id);
+        return accountById.get().getBalance();
     }
 }
